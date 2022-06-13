9da8711c281a074f78e761d392ad829cec0e3537
broadnetty <mikhail.topskiy@gmail.com>
2022-06-13 17:52:02 +0400

    classes

--- main-test.py
+++ main-test.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 import requests
 from yacloud import yacloud
+yacm = yacloud.yacloudmanager()
 
+print(yacm.listClouds())
+"""
 encoded_token = yacloud.obtainIAMtoken()
 
 cloudId = yacloud.listClouds(encoded_token)['clouds'][0]['id']
 
-print(id)
+print(cloudId)
 
+folders = yacloud.listFolders(encoded_token, cloudId)['folders']
 
+print(folders)
 
+for folder in folders:
+    if folder['name'] == 'mtop-test-cloud':
+        print(yacloud.listInstances(encoded_token, folder['id']))
+"""
--- yacloud/yacloud.py
+++ yacloud/yacloud.py
@@ -1,56 +1,92 @@
 import requests
-import time, jwt
+import time, datetime, jwt
 
-def obtainIAMtoken(JWTtoken=None):
-    try:
-        if JWTtoken == None:
-            print("obtaining token")
-            headers = { 'Metadata-Flavor': 'Google' }
-            r = requests.get('http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token', headers=headers, timeout=5)
+class yacloudmanager:
 
-        return r.json()["access_token"]
-    except:
-        service_account_id = "ajegng4l26ojveihgs0c"
-        key_id = "aje5b9tqno8kbkeq3d9m"  # ID ресурса Key, который принадлежит сервисному аккаунту.
+    IAMtoken = None
+    tokenExpiration = None
+    APItimeout = 5
+    isIAMrole = True
+    headers = {'Metadata-Flavor': 'Google'}
 
-        with open("backup-account-key.pem", 'r') as private:
-            private_key = private.read()  # Чтение закрытого ключа из файла.
+    def __init__(self):
+        self.IAMtoken = self.obtainIAMtoken()
+        self.headers = {'Metadata-Flavor': 'Google', 'Authorization': 'Bearer ' + self.IAMtoken}
+        return
 
-        now = int(time.time())
-        payload = {
-            'aud': 'https://iam.api.cloud.yandex.net/iam/v1/tokens',
-            'iss': service_account_id,
-            'iat': now,
-            'exp': now + 360}
+    #checks if the token is still valid or expired
+    def checkTokenAlive(self):
+        if self.tokenExpiration - datetime.datetime.utcnow() > datetime.timedelta(seconds=60):
+            return self.tokenExpiration
+        else:
+            self.headers = {'Metadata-Flavor': 'Google'}
+            return self.obtainIAMtoken()
 
-        encoded_token = jwt.encode(
-            payload,
-            private_key,
-            algorithm='PS256',
-            headers={'kid': key_id})
-        print("JWT: " + encoded_token)
+    def obtainIAMtoken(self, JWTtoken=None):
+        try:
+            if JWTtoken == None:
+                r = requests.get('http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token',
+                                 headers=self.headers, timeout=self.APItimeout)
 
-        headers = {'Content-Type': 'application/json'}
-        r = requests.post('https://iam.api.cloud.yandex.net/iam/v1/tokens',
-                         headers=headers, json={"jwt": encoded_token}, timeout=5)
-        print(r.text)
-        return r.json()["iamToken"]
+            return r.json()["access_token"]
 
-def listOrganizations(IAMtoken):
-    headers = {'Metadata-Flavor': 'Google', 'Authorization': 'Bearer ' + IAMtoken}
-    r = requests.get('https://organization-manager.api.cloud.yandex.net/organization-manager/v1/organizations',
-                     headers=headers, timeout=5)
-    return r.json()
+        except:
 
-def listClouds(IAMtoken):
-    headers = {'Metadata-Flavor': 'Google', 'Authorization': 'Bearer ' + IAMtoken}
-    r = requests.get('https://resource-manager.api.cloud.yandex.net/resource-manager/v1/clouds',
-                     headers=headers, timeout=5)
-    return r.json()
+            self.isIAMrole = False
+            service_account_id = "ajegng4l26ojveihgs0c"
+            key_id = "aje5b9tqno8kbkeq3d9m"  # ID ресурса Key, который принадлежит сервисному аккаунту.
+
+            with open("backup-account-key.pem", 'r') as private:
+                private_key = private.read()  # Чтение закрытого ключа из файла.
+
+            now = int(time.time())
+            payload = {
+                'aud': 'https://iam.api.cloud.yandex.net/iam/v1/tokens',
+                'iss': service_account_id,
+                'iat': now,
+                'exp': now + 360}
+
+            encoded_token = jwt.encode(
+                payload,
+                private_key,
+                algorithm='PS256',
+                headers={'kid': key_id})
+
+            headers = {'Content-Type': 'application/json'}
+            r = requests.post('https://iam.api.cloud.yandex.net/iam/v1/tokens',
+                              headers=headers, json={"jwt": encoded_token}, timeout=self.APItimeout)
 
 
-def listFolders(IAMtoken,cloudId):
-    headers = {'Metadata-Flavor': 'Google', 'Authorization': 'Bearer ' + IAMtoken}
-    r = requests.get('https://resource-manager.api.cloud.yandex.net/resource-manager/v1/folders', params={'cloudId': cloudId},
-                     headers=headers, timeout=5)
-    return r.json()
+            self.tokenExpiration = datetime.datetime.fromisoformat(r.json()["expiresAt"].split('.')[0])
+            return r.json()["iamToken"]
+
+    def listClouds(self):
+        self.checkTokenAlive()
+        r = requests.get('https://resource-manager.api.cloud.yandex.net/resource-manager/v1/clouds',
+                         headers=self.headers, timeout=self.APItimeout)
+        return r.json()
+
+    def listOrganizations(self):
+        self.checkTokenAlive()
+        r = requests.get('https://organization-manager.api.cloud.yandex.net/organization-manager/v1/organizations',
+                         headers=self.headers, timeout=self.APItimeout)
+        return r.json()
+
+    def listClouds(self):
+        self.checkTokenAlive()
+        r = requests.get('https://resource-manager.api.cloud.yandex.net/resource-manager/v1/clouds',
+                         headers=self.headers, timeout=self.APItimeout)
+        return r.json()
+
+    def listFolders(self, cloudId):
+        self.checkTokenAlive()
+        r = requests.get('https://resource-manager.api.cloud.yandex.net/resource-manager/v1/folders',
+                         params={'cloudId': cloudId},
+                         headers=self.headers, timeout=self.APItimeout)
+        return r.json()
+
+    def listInstances(self, folderId):
+        self.checkTokenAlive()
+        r = requests.get('https://compute.api.cloud.yandex.net/compute/v1/instances', params={'folderId': folderId},
+                         headers=self.headers, timeout=self.APItimeout)
+        return r.json()
